fb6cdf23c1e0caf99ca2e8fea904640c0914f6e0
weijie_huang <jaycekon@163.com>
2017-09-20 15:18:52 +0800

    Spring boot web 项目搭建

---
+++ spring-boot-web/src/main/java/com/jaycekon/demo/SpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.jaycekon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+}
