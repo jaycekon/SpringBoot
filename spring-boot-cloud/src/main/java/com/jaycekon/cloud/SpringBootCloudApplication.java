d5cf6dd2eb6da71395c39f8773fbe6a483831443
jaycekon@163.com <jaycekong824>
2017-09-20 21:41:42 +0800

    Spring cloud 学习demo

--- spring-boot-cloud/src/main/java/com/jaycekon/cloud/SpringBootCloudApplication.java
+++ spring-boot-cloud/src/main/java/com/jaycekon/cloud/SpringBootCloudApplication.java
@@ -2,8 +2,10 @@ package com.jaycekon.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringBootCloudApplication {
 
 	public static void main(String[] args) {
