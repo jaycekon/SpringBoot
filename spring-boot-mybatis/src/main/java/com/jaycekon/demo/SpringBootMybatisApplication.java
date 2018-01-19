cb0c056ef384b111fcd7b9afec58fecf03531f18
weijie_huang <jaycekon@163.com>
2018-01-19 10:14:00 +0800

    :zap:加入Spring-Boot-Mybatis 模块

---
+++ spring-boot-mybatis/src/main/java/com/jaycekon/demo/SpringBootMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.jaycekon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisApplication.class, args);
+	}
+}
