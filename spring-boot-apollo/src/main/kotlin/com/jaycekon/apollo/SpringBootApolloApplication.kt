491bed81ecd6c055f9d67de9a269c19bec141c34
huangweijie <huangweijie@cvte.com>
2018-07-14 11:16:57 +0800

    :fire: spring-boot 给予Kotlin 整合Apollo

---
+++ spring-boot-apollo/src/main/kotlin/com/jaycekon/apollo/SpringBootApolloApplication.kt
@@ -0,0 +1,13 @@
+package com.jaycekon.apollo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+
+@SpringBootApplication
+@ComponentScan(value = "com.jaycekon.apollo")
+open class SpringBootApolloApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootApolloApplication>(*args)
+}
---
+++ spring-boot-apollo/src/test/kotlin/com/jaycekon/apollo/SpringBootApolloApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.jaycekon.apollo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootApolloApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
