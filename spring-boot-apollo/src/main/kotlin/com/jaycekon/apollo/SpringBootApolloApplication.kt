fcf5e84bd51413284a286b79f94dec551f25fded
huangweijie <huangweijie@cvte.com>
2018-07-16 11:28:19 +0800

    :zap: fix Spring config

--- spring-boot-apollo/src/main/kotlin/com/jaycekon/apollo/SpringBootApolloApplication.kt
+++ spring-boot-apollo/src/main/kotlin/com/jaycekon/apollo/SpringBootApolloApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(value = "com.jaycekon.apollo")
+@ComponentScan(value = ["com.jaycekon.apollo"])
 open class SpringBootApolloApplication
 
 fun main(args: Array<String>) {
